79db284c91f3be2c561e8c51289e49dd3b42ee2a
Sebastian Hinterauer <fhs45884@fh-salzburg.ac.at>
2022-01-20 15:32:24 +0100

    Made url changes

--- src/php/index.php
+++ src/php/index.php
@@ -46,7 +46,7 @@
                 <li><a class="button-like" href="#">Über mich</a></li>
             </ul>-->
 
-            <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+            <?php wp_nav_menu(array('theme_location' => 'main-menu')); ?>
         </nav>
         <a href="#" id="button"><span></span></a>
 
@@ -58,121 +58,116 @@
 
     <main>
         <?php if (is_front_page()) { ?>
-        <section id="news">
-            <h2 class="headline">News</h2>
-            <div class="divider">
-                <div class="noShow">
-                    <img class="newsPic" src="<?php bloginfo('template_directory');?>/images/erdhand.jpg" alt="Hand mit Erde">
-                </div>
-
-                <div class="div-padding">
-                    <ul>
-                        <?php
-                        $news_query = new WP_Query( 'order=DESC&category_name=news' ); 
-
-                        if ( $news_query->have_posts() ){
-                            $COUNT = 1;
-                            while ( $news_query->have_posts() && $COUNT <= 3) {
-                            $news_query->the_post(); ?>
-                            <li>
-                                <a href="<?php the_permalink();?>"><?php the_title(); ?></a>
-                                <p><?php the_content(); ?></p>
-                            </li>
-                            <?php $COUNT++;
-                        } ?>
-                        <?php } 
-                        else {
-                        ?>
-                            <p>Erster Post demnächst...</p>
-                        <?php }?>
-                        <? wp_reset_postdata();?> 
-                    </ul>
-
-                    <a class="button-like button-news" href="/?page_id=141">Alle Anzeigen</a>
-
-                </div>
-            </div>
-
-        </section>
-
-        <section id="erfolge">
-            <h2 class="headline">Erfolge</h2>
-            <div class="divider div-padding">
-
-                <div>
-                    <div class="container">
-
-                    <?php $services_query = new WP_Query( 'order=ASC&category_name=erfolge' ); 
-            
-                    if ( $services_query->have_posts() ){  
-                        $COUNT = 1;
-                    
-                        while ( $services_query->have_posts() && $COUNT <= 3) {
-                            $services_query->the_post();
-                            $thumbnail_id = get_post_thumbnail_id();?>
-                            <a href="<?php the_permalink();?>">
-                                <div class="card">
-                                    <div class="div-pic">
-                                        <img class="cardPic" src="<?php echo wp_get_attachment_image_url($thumbnail_id);?>" alt="<?php get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);?>">
-                                    </div>
-                                    <div class="div-erfindername">
-                                        <p><?php the_title();?></p>
-                                    </div>
-                                </div>
-                            </a>
-                    <?php $COUNT++;
-                            wp_reset_postdata();
-                        }?>
-                    <?php }
-                    else {?>
-                        <p>Keine Referenzen vorhanden...</p>
-                    <?php }?>
+            <section id="news">
+                <h2 class="headline">News</h2>
+                <div class="divider">
+                    <div class="noShow">
+                        <img class="newsPic" src="<?php bloginfo('template_directory'); ?>/images/erdhand.jpg" alt="Hand mit Erde">
                     </div>
-                    <a class="button-like button-news" href="/?page_id=139">Alle Anzeigen</a>
+
+                    <div class="div-padding">
+                        <ul>
+                            <?php
+                            $news_query = new WP_Query('order=DESC&category_name=news');
+
+                            if ($news_query->have_posts()) {
+                                $COUNT = 1;
+                                while ($news_query->have_posts() && $COUNT <= 3) {
+                                    $news_query->the_post(); ?>
+                                    <li>
+                                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                        <p><?php the_content(); ?></p>
+                                    </li>
+                                <?php $COUNT++;
+                                } ?>
+                            <?php } else {
+                            ?>
+                                <p>Erster Post demnächst...</p>
+                            <?php } ?>
+                            <? wp_reset_postdata(); ?>
+                        </ul>
+
+                        <a class="button-like button-news" href="/news">Alle Anzeigen</a>
+
+                    </div>
                 </div>
 
+            </section>
 
-                <div class="div-padding noShow">
-                    <img class="erfolgsPic" src="<?php bloginfo('template_directory');?>/images/giesen.jpg" alt="Gieskanne gießt Beet.">
+            <section id="erfolge">
+                <h2 class="headline">Erfolge</h2>
+                <div class="divider div-padding">
+
+                    <div>
+                        <div class="container">
+
+                            <?php $services_query = new WP_Query('order=ASC&category_name=erfolge');
+
+                            if ($services_query->have_posts()) {
+                                $COUNT = 1;
+
+                                while ($services_query->have_posts() && $COUNT <= 3) {
+                                    $services_query->the_post();
+                                    $thumbnail_id = get_post_thumbnail_id(); ?>
+                                    <a href="<?php the_permalink(); ?>">
+                                        <div class="card">
+                                            <div class="div-pic">
+                                                <img class="cardPic" src="<?php echo wp_get_attachment_image_url($thumbnail_id); ?>" alt="<?php get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true); ?>">
+                                            </div>
+                                            <div class="div-erfindername">
+                                                <p><?php the_title(); ?></p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                <?php $COUNT++;
+                                    wp_reset_postdata();
+                                } ?>
+                            <?php } else { ?>
+                                <p>Keine Referenzen vorhanden...</p>
+                            <?php } ?>
+                        </div>
+                        <a class="button-like button-news" href="/erfolge">Alle Anzeigen</a>
+                    </div>
+
+
+                    <div class="div-padding noShow">
+                        <img class="erfolgsPic" src="<?php bloginfo('template_directory'); ?>/images/giesen.jpg" alt="Gieskanne gießt Beet.">
+                    </div>
                 </div>
-            </div>
 
-        </section>
+            </section>
 
-        <section id="me">
-            <h2 class="headline">Das bin ich</h2>
-            <div class="divider">
-                <div class="div-michtext">
-                    <p>
-                        <?php
-                        $about_query = new WP_Query( 'order=ASC&category_name=aboutme' ); 
+            <section id="me">
+                <h2 class="headline">Das bin ich</h2>
+                <div class="divider">
+                    <div class="div-michtext">
+                        <p>
+                            <?php
+                            $about_query = new WP_Query('order=ASC&category_name=aboutme');
 
-                        if ( $about_query->have_posts() ){
-                            the_content();
-                        } 
-                        else {
-                            echo "Erster Post demnächst...";
-                        }?>
-                    </p>
+                            if ($about_query->have_posts()) {
+                                the_content();
+                            } else {
+                                echo "Erster Post demnächst...";
+                            } ?>
+                        </p>
+                    </div>
+
+                    <div class="div-padding noShow">
+
+                        <img class="mePic" src="<?php bloginfo('template_directory'); ?>/images/binich.png" alt="Andy beim Schrauben">
+                    </div>
                 </div>
 
-                <div class="div-padding noShow">
-
-                    <img class="mePic" src="<?php bloginfo('template_directory');?>/images/binich.png" alt="Andy beim Schrauben">
-                </div>
-            </div>
-
-        </section>
-        <?php }
-        else {
+            </section>
+        <?php } else {
             if (have_posts()) {
                 while (have_posts()) {
                     the_post();
                     the_title();
                     the_content();
                 }
-            }
-            else {
+            } else {
                 echo "<p>Derzeit noch keine Beiträge... Bitte kommen Sie später weider zurück!</p>";
             }
         } ?>
@@ -180,7 +175,7 @@
 
     <?php include('_footer.php') ?>
 
-    <script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/burger.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/burger.js"></script>
 </body>
 
 </html>
