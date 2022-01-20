a4fd069180ff6e7a5b4df23632a97a085e2128c2
Sebastian Hinterauer <fhs45884@fh-salzburg.ac.at>
2022-01-20 15:13:21 +0100

    Removed not used themes

--- public/wp-content/themes/alex/_footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-        <img class="logos" src="<?php bloginfo('template_directory');?>/images/glyph-logo_May2016.png" alt="Instagramm Logo">
-        <img class="logos" src="<?php bloginfo('template_directory');?>/images/2021-twitter-logo-black.png" alt="Twitter Logo">
-</footer>
-    <?php wp_footer(); ?>
